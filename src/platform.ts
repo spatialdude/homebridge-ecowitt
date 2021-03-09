5851737493e473105cc0cd681d5eaef466b7f821
Elvin Slavik <eslavik@esri.com>
2021-03-09 21:59:25 +1100

    Initial version

--- src/platform.ts
+++ src/platform.ts
@@ -1,25 +1,85 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+//import { EcowittPlatformAccessory } from './platformAccessory';
+import { WH25, WH65, WH57, EcowittPlatformAccessory } from './platformAccessory';
+
+import * as restify from 'restify';
+import * as crypto from 'crypto';
+import { timeStamp } from 'node:console';
+
+const SensorClasses = {
+  WH25: EcowittPlatformAccessory,
+  WH65: EcowittPlatformAccessory,
+  WH57: EcowittPlatformAccessory,
+};
+
+const SensorInfos = {
+  WH25: {
+    name: 'WH25',
+    displayName: 'Indoor temperature, humidity & pressure sensor',
+  },
+  WH65: {
+    name: 'WH65',
+    displayName: 'Outdor 7 in 1 sensor group',
+  },
+  WH57: {
+    name: 'WH57',
+    displayName: 'Lightning sensor',
+  },
+};
 
 /**
  * HomebridgePlatform
  * This class is the main constructor for your plugin, this is where you should
  * parse the user config and discover/register accessories with Homebridge.
  */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class EcowittHomebridgePlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
   // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
 
+  public wxDataReportServer: restify.Server;
+  public wxDataReport = null;
+
+  public wxStationInfo = {
+    manufacturer: 'Ecowitt',
+    model: '',
+    serialNumber: this.config.mac,
+    softwareRevision: '',
+    frequency: '',
+    PASSKEY: crypto
+      .createHash('md5')
+      .update(this.config.mac)
+      .digest('hex').toUpperCase(),
+    sensors: [],
+    sensorInstances: {},
+  };
+
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
+
+    this.wxDataReportServer = restify.createServer();
+    this.wxDataReportServer.use(restify.plugins.bodyParser());
+
+    this.log.info('Data report path:', this.config.path);
+    this.log.info('Data report port:', this.config.port);
+
+    this.wxDataReportServer.post(
+      this.config.path,
+      (req, res, next) => {
+        this.log.info('Data source address:', req.socket.remoteAddress);
+        this.log.info('Request:', req.toString());
+        this.wxDataReportReport(req.body);
+        next();
+      });
+
+
     this.log.debug('Finished initializing platform:', this.config.name);
 
     // When this event is fired it means Homebridge has restored all cached accessories from disk.
@@ -28,8 +88,10 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     // to start discovery of new accessories.
     this.api.on('didFinishLaunching', () => {
       log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
-      this.discoverDevices();
+
+      this.wxDataReportServer.listen(this.config.port, () => {
+        this.log.info('Listening at %s', this.wxDataReportServer.url);
+      });
     });
   }
 
@@ -44,37 +106,50 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     this.accessories.push(accessory);
   }
 
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
-  discoverDevices() {
 
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
+  wxDataReportReport(dataReport) {
+    if (dataReport.PASSKEY !== this.wxStationInfo.PASSKEY) {
+      this.log.error('Report not for this station:', JSON.stringify(dataReport, undefined, 2));
+      return;
+    }
 
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+    this.log.info('Data report:', JSON.stringify(dataReport, undefined, 2));
 
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+    if (!this.wxDataReport) {
+      this.wxDataReport = dataReport;
+      this.wxRegisterAccessories(dataReport);
+    } else {
+      this.wxDataReport = dataReport;
+    }
 
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
+    this.wxUpdateAccessories(dataReport);
+  }
+
+  addSensorType(add, name) {
+    if (add) {
+      this.wxStationInfo.sensors.push(name);
+    }
+  }
+
+  wxRegisterAccessories(dataReport) {
+    this.wxStationInfo.model = dataReport.model;
+    this.wxStationInfo.softwareRevision = dataReport.stationtype;
+    this.wxStationInfo.frequency = dataReport.freq;
+
+    this.addSensorType(dataReport['wh25batt'] !== undefined, 'WH25');
+    this.addSensorType(dataReport['wh65batt'] !== undefined, 'WH65');
+    this.addSensorType(dataReport['wh57batt'] !== undefined, 'WH57');
+
+    this.log.info('WX Station:', JSON.stringify(this.wxStationInfo, undefined, 2));
+
+    for (const sensor of this.wxStationInfo.sensors) {
+      const sensorId = this.config.mac + '_' + sensor;
+      const uuid = this.api.hap.uuid.generate(sensorId);
+
+      this.log.info('Sensor:', sensor, sensorId, 'uuid:', uuid);
+
+      const sensorInfo = SensorInfos[sensor];
+
       const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
 
       if (existingAccessory) {
@@ -87,30 +162,66 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
+        //new EcowittPlatformAccessory(this, existingAccessory);
 
         // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
         // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
+        this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+        this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+        //} else {
+      }
+      {
         // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+        this.log.info('Adding new accessory:', sensor);
 
         // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+        const accessory = new this.api.platformAccessory(sensorInfo.name, uuid);
 
         // store a copy of the device object in the `accessory.context`
         // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
+        accessory.context.sensorInfo = sensorInfo;
 
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
+        //new EcowittPlatformAccessory(this, accessory);
+
+        let instance;
+
+        switch (sensor) {
+          case 'WH25':
+            instance = new WH25(this, accessory);
+            break;
+
+          case 'WH65':
+            instance = new WH65(this, accessory);
+            break;
+
+          case 'WH57':
+            instance = new WH57(this, accessory);
+            break;
+
+          default:
+            instance = new EcowittPlatformAccessory(this, accessory);
+            break;
+        }
+
+        this.wxStationInfo.sensorInstances[sensor] = instance;
 
         // link the accessory to your platform
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
+    }
+  }
+
+  wxUpdateAccessories(dataReport) {
+    const dateUTC = new Date(dataReport.dateutc);
+    this.log.info('Report time:', dateUTC);
+
+    for (const sensor of this.wxStationInfo.sensors) {
+      const instance = this.wxStationInfo.sensorInstances[sensor];
+
+      this.log.info('Updating:', sensor, !!instance);
+      instance.update(dataReport);
     }
   }
 }
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,141 +1,326 @@
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import { inherits } from 'node:util';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { EcowittHomebridgePlatform } from './platform';
 
-/**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
- */
-export class ExamplePlatformAccessory {
-  private service: Service;
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  };
+export class EcowittPlatformAccessory {
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
-    private readonly accessory: PlatformAccessory,
+    protected readonly platform: EcowittHomebridgePlatform,
+    protected readonly accessory: PlatformAccessory,
   ) {
 
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
-
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
-
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
-
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
-
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
-
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
-
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
-
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, platform.wxStationInfo.manufacturer)
+      .setCharacteristic(this.platform.Characteristic.ProductData, platform.wxStationInfo.frequency)
+      .setCharacteristic(this.platform.Characteristic.Model, platform.wxStationInfo.model)
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, platform.wxStationInfo.serialNumber)
+      .setCharacteristic(this.platform.Characteristic.HardwareRevision, platform.wxStationInfo.model)
+      .setCharacteristic(this.platform.Characteristic.SoftwareRevision, platform.wxStationInfo.softwareRevision);
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+  addBattery(name: string) {
+    const battery = this.accessory.getService(this.platform.Service.Battery)
+      || this.accessory.addService(this.platform.Service.Battery);
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+    battery.setCharacteristic(
+      this.platform.Characteristic.Name,
+      name || (this.accessory.context.sensorInfo.name + ' Battery'));
+
+    battery.setCharacteristic(
+      this.platform.Characteristic.ChargingState,
+      this.platform.Characteristic.ChargingState.NOT_CHARGEABLE);
+
+    battery.setCharacteristic(
+      this.platform.Characteristic.StatusLowBattery,
+      this.platform.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL);
+
+    return battery;
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
-
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return isOn;
+  toCelcius(fahrenheit: any): number {
+    return (parseFloat(fahrenheit) - 32) * 5 / 9;
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+  tohPa(inHg: any): number {
+    return parseFloat(inHg) * 33.8638;
+  }
+}
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+export class WH25 extends EcowittPlatformAccessory {
+  protected temperatureSensor: Service;
+  protected humiditySensor: Service;
+  protected absolutePressureSensor: Service;
+  protected relativePressureSensor: Service;
+
+  constructor(
+    protected readonly platform: EcowittHomebridgePlatform,
+    protected readonly accessory: PlatformAccessory,
+  ) {
+    super(platform, accessory);
+
+    this.temperatureSensor = this.accessory.getService(this.platform.Service.TemperatureSensor)
+      || this.accessory.addService(this.platform.Service.TemperatureSensor);
+
+    this.temperatureSensor.setCharacteristic(
+      this.platform.Characteristic.Name,
+      'Indoor Temperature');
+
+    this.humiditySensor = this.accessory.getService(this.platform.Service.HumiditySensor)
+      || this.accessory.addService(this.platform.Service.HumiditySensor);
+
+    this.humiditySensor.setCharacteristic(
+      this.platform.Characteristic.Name,
+      'Indoor Humidity');
+
+    this.absolutePressureSensor = this.accessory.getService(this.platform.Service.OccupancySensor)
+      || this.accessory.addService(this.platform.Service.OccupancySensor);
+
+    this.relativePressureSensor = this.accessory.getService(this.platform.Service.MotionSensor)
+      || this.accessory.addService(this.platform.Service.MotionSensor);
   }
 
+  update(dataReport) {
+    this.platform.log.info('WH25 Update');
+    this.platform.log.info('  wh25batt:', dataReport.wh25batt);
+    this.platform.log.info('  tempinf:', dataReport.tempinf);
+    this.platform.log.info('  humidityin:', dataReport.humidityin);
+    this.platform.log.info('  baromrelin', dataReport.baromrelin);
+    this.platform.log.info('  baromabsin', dataReport.baromabsin);
+
+    const lowBattery = dataReport.wh25batt == '1'
+      ? this.platform.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW
+      : this.platform.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+
+    // Temperature
+
+    this.temperatureSensor.updateCharacteristic(
+      this.platform.Characteristic.CurrentTemperature,
+      this.toCelcius(dataReport.tempinf));
+
+    this.temperatureSensor.updateCharacteristic(
+      this.platform.Characteristic.StatusLowBattery,
+      lowBattery);
+
+    // Humidity
+
+    this.humiditySensor.updateCharacteristic(
+      this.platform.Characteristic.CurrentRelativeHumidity,
+      parseFloat(dataReport.humidityin));
+
+    this.humiditySensor.updateCharacteristic(
+      this.platform.Characteristic.StatusLowBattery,
+      lowBattery);
+
+    // Barometer
+
+    this.absolutePressureSensor.updateCharacteristic(
+      this.platform.Characteristic.Name,
+      Math.round(this.tohPa(dataReport.baromabsin)).toString() + ' hPa (Abs)');
+
+    this.absolutePressureSensor.updateCharacteristic(
+      this.platform.Characteristic.StatusLowBattery,
+      lowBattery);
+
+    this.relativePressureSensor.updateCharacteristic(
+      this.platform.Characteristic.Name,
+      Math.round(this.tohPa(dataReport.baromrelin)).toString() + ' hPa (Rel)');
+
+    this.relativePressureSensor.updateCharacteristic(
+      this.platform.Characteristic.StatusLowBattery,
+      lowBattery);
+  }
+}
+
+export class WH65 extends EcowittPlatformAccessory {
+  protected temperatureSensor: Service;
+  protected humiditySensor: Service;
+
+  constructor(
+    protected readonly platform: EcowittHomebridgePlatform,
+    protected readonly accessory: PlatformAccessory,
+  ) {
+    super(platform, accessory);
+
+    this.temperatureSensor = this.accessory.getService(this.platform.Service.TemperatureSensor)
+      || this.accessory.addService(this.platform.Service.TemperatureSensor);
+
+    this.temperatureSensor.setCharacteristic(
+      this.platform.Characteristic.Name,
+      'Outdoor Temperature');
+
+    this.humiditySensor = this.accessory.getService(this.platform.Service.HumiditySensor)
+      || this.accessory.addService(this.platform.Service.HumiditySensor);
+
+    this.humiditySensor.setCharacteristic(
+      this.platform.Characteristic.Name,
+      'Outdoor Humidity');
+  }
+
+  update(dataReport) {
+    this.platform.log.info('WH65 Update');
+    this.platform.log.info('  wh65batt:', dataReport.wh65batt);
+    this.platform.log.info('  tempf:', dataReport.tempf);
+    this.platform.log.info('  humidity:', dataReport.humidity);
+
+    const lowBattery = dataReport.wh65batt == '1'
+      ? this.platform.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW
+      : this.platform.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+
+    this.temperatureSensor.updateCharacteristic(
+      this.platform.Characteristic.CurrentTemperature,
+      this.toCelcius(dataReport.tempf));
+
+    this.temperatureSensor.updateCharacteristic(
+      this.platform.Characteristic.StatusLowBattery,
+      lowBattery);
+
+    this.humiditySensor.updateCharacteristic(
+      this.platform.Characteristic.CurrentRelativeHumidity,
+      parseFloat(dataReport.humidity));
+
+    this.humiditySensor.updateCharacteristic(
+      this.platform.Characteristic.StatusLowBattery,
+      lowBattery);
+  }
+}
+
+export class WH57 extends EcowittPlatformAccessory {
+  protected battery: Service;
+  protected lightningSensor: Service;
+  protected lightningDistance: Service;
+  protected lightningCount: Service;
+
+  constructor(
+    protected readonly platform: EcowittHomebridgePlatform,
+    protected readonly accessory: PlatformAccessory,
+  ) {
+    super(platform, accessory);
+
+    this.lightningDistance = this.accessory.getService(this.platform.Service.OccupancySensor)
+      || this.accessory.addService(this.platform.Service.OccupancySensor);
+
+    this.lightningCount = this.accessory.getService(this.platform.Service.MotionSensor)
+      || this.accessory.addService(this.platform.Service.MotionSensor);
+
+    this.lightningSensor = this.accessory.getService(this.platform.Service.ContactSensor)
+      || this.accessory.addService(this.platform.Service.ContactSensor);
+
+    this.battery = this.addBattery('⚡Battery');
+  }
+
+  update(dataReport) {
+    this.platform.log.info('WH57 Update');
+    this.platform.log.info('  wh57batt:', dataReport.wh57batt);
+    this.platform.log.info('  lightning:', dataReport.lightning);
+    this.platform.log.info('  lightning_num:', dataReport.lightning_num);
+    this.platform.log.info('  lightning_time:', dataReport.lightning_time);
+
+    // Battery
+
+    const batteryLevel = parseFloat(dataReport.wh57batt) / 5;
+    const lowBattery = batteryLevel <= 0.2
+      ? this.platform.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW
+      : this.platform.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+
+    this.battery.updateCharacteristic(
+      this.platform.Characteristic.StatusLowBattery,
+      lowBattery);
+
+    this.battery.updateCharacteristic(
+      this.platform.Characteristic.BatteryLevel,
+      batteryLevel * 100);
+
+    // Detection time
+
+    const lightningTime = dataReport.lightning_time > ''
+      ? parseInt(dataReport.lightning_time)
+      : 0;
+
+    const lightningDetected = lightningTime > 0 && lightningTime < 300000; // < 5 min
+
+    this.lightningSensor.updateCharacteristic(
+      this.platform.Characteristic.ContactSensorState,
+      !lightningDetected
+        ? this.platform.Characteristic.ContactSensorState.CONTACT_DETECTED
+        : this.platform.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED);
+
+    this.lightningSensor.updateCharacteristic(
+      this.platform.Characteristic.StatusLowBattery,
+      lowBattery);
+
+    let contactText = '';
+
+    if (lightningTime) {
+      const ms = lightningTime;
+      let s = Math.floor(ms / 1000);
+      let m = Math.floor(s / 60);
+      s = s % 60;
+      let h = Math.floor(m / 60);
+      m = m % 60;
+      const d = Math.floor(h / 24);
+      h = h % 24;
+
+      // eslint-disable-next-line no-inner-declarations
+      function append(t: string, sep = ',') {
+        if (contactText) {
+          contactText += sep;
+        }
+
+        contactText += t;
+      }
+
+      if (d > 1) {
+        append(d.toString() + ' days');
+      } else if (d || h || m) {
+        if (d) {
+          append(d > 1 ? d.toString() + ' days' : '1 day');
+        }
+        if (h) {
+          append(h > 1 ? h.toString() + ' hours' : '1 hour');
+        }
+        if (m && !d) {
+          append(m > 1 ? m.toString() + ' minutes' : '1 minute');
+        }
+      } else if (s) {
+        append(m > 1 ? m.toString() + ' seconds' : '1 second');
+      }
+
+      contactText += ' ago';
+    }
+
+    this.lightningSensor.updateCharacteristic(
+      this.platform.Characteristic.Name,
+      contactText);
+
+    // Distance to lightning
+
+    this.lightningDistance.updateCharacteristic(
+      this.platform.Characteristic.Name,
+      `⚡Distance: ${dataReport.lightning} km`);
+
+    this.lightningDistance.updateCharacteristic(
+      this.platform.Characteristic.OccupancyDetected,
+      lightningDetected);
+
+    this.lightningDistance.updateCharacteristic(
+      this.platform.Characteristic.StatusLowBattery,
+      lowBattery);
+
+    // Number of lightning strikes
+
+    this.lightningCount.updateCharacteristic(
+      this.platform.Characteristic.MotionDetected,
+      lightningDetected);
+
+    this.lightningCount.updateCharacteristic(
+      this.platform.Characteristic.StatusLowBattery,
+      lowBattery);
+
+    this.lightningCount.updateCharacteristic(
+      this.platform.Characteristic.Name,
+      `⚡Strikes: ${dataReport.lightning_num}`);
+  }
 }
