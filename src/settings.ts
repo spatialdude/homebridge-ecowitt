b3a7403651f12a27f87b413c5bdc0a3241d70e68
Marton Bodonyi <marty@interactivejunky.com>
2023-09-06 22:31:51 +1000

    Fix plugin name for forked version

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'Ecowitt';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-ecowitt';
+export const PLUGIN_NAME = 'homebridge-ecowitt-node-18';
